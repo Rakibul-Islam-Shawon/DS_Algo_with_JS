fa1dfc1d1e958817a89951248c933114adbe9343
Md Rakibul Islam <shawonnstu@gmail.com>
2024-09-13 12:38:28 +0200

    Change a little bit

--- Sum_of_digits.js
+++ Sum_of_digits.js
@@ -38,3 +38,21 @@ function numberOfDigits(num){
 }
 
 console.log(numberOfDigits(-145972));
+
+
+//IsPalindrome ? 
+
+function isPalindrome(x){
+    let copyNum = x;
+    let reverseNum = 0;
+
+    while(copyNum>0){
+        const lastDigit = copyNum % 10;
+        reverseNum = reverseNum * 10 + lastDigit;
+        copyNum = Math.floor(copyNum/10);
+    }
+
+    return (x === reverseNum);
+}
+
+console.log(isPalindrome(1314));
