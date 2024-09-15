b3de1c6c0f203ae98af09134391812f2eac9b778
Md Rakibul Islam <shawonnstu@gmail.com>
2024-09-15 21:06:27 +0200

    First and last element finding

--- Array.js
+++ Array.js
@@ -1,3 +1,17 @@
 let arr = [1,2,3,"Nice", 2.3,{name: "Rakib"}, [2,3]];
 
+
+//Length of an array
+let arrLength = arr.length;
+console.log(arrLength);
+
+
 console.log(arr[5].name);
+
+
+//access of first element and last element of an array
+
+let firstElement = arr[0];
+let lastElement = arr[arrLength-1];
+
+console.log(firstElement, lastElement[0]);
