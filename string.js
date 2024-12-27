a7eaef20ad4c18414aba6070df638c064198b6e2
Rakibul Islam Shawon <rakibicenstu@gmail.com>
2024-12-27 23:33:48 +0100

    Change in string

--- string.js
+++ string.js
@@ -1,11 +1,14 @@
 
-let name1 = "Rakib is a student";
+// let name1 = "Rakib is a student";
 
-console.log(name1);
-console.log(name1.length);
-console.log(name1.at(1));
-console.log(name1.charAt(3));
-console.log(name1.indexOf("b"));
-console.log(name1.search("is"));
-console.log(name1.substring("is"));
+// console.log(name1);
+// console.log(name1.length);
+// console.log(name1.at(1));
+// console.log(name1.charAt(3));
+// console.log(name1.indexOf("b"));
+// console.log(name1.search("is"));
+// console.log(name1.substring("is"));
 
+const name_a = "W3Schools";
+let letter = name_a.at(2);
+console.log(letter);
