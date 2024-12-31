436f9f796656aa3da7699e06e24803902684c7af
Rakibul Islam Shawon <rakibicenstu@gmail.com>
2024-12-31 22:58:53 +0100

    Join method added

--- ArrayFunction.js
+++ ArrayFunction.js
@@ -1,2 +1,8 @@
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 console.log(fruits.includes("Mango"));
+
+
+
+
+const fruitsAll = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruitsAll.join(" * "));
