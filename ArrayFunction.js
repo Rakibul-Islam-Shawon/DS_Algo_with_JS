3e5fdb3d81429aa6c18e7efd7e20988fb6e77f99
Rakibul Islam Shawon <rakibicenstu@gmail.com>
2025-01-04 23:31:56 +0100

    array function file changed

--- ArrayFunction.js
+++ ArrayFunction.js
@@ -8,13 +8,13 @@ const fruitsAll = ["Banana", "Orange", "Apple", "Mango","nashpati","Watermelon"]
 console.log(fruitsAll.join(" * "));
 
 
-//Array
-// const newFruits = ["Apple", "Orange", "Apple", "Mango"];
-// let position = newFruits.indexOf("Orange") + 1;
-
-// console.log(position);
-
-
+Array
 const newFruits = ["Apple", "Orange", "Apple", "Mango"];
-let position = newFruits.lastIndexOf("Apple") + 1;
+let position = newFruits.indexOf("Orange") + 1;
+
 console.log(position);
+
+
+const newFruits1 = ["Apple", "Orange", "Apple", "Mango"];
+let position1 = newFruits1.lastIndexOf("Apple") + 1;
+console.log(position1);
