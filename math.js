1d3549bf5a2e5d09b9d798faad6c2e52766c582c
Rakibul Islam <rakibicenstu@gmail.com>
2025-03-08 15:40:40 +0100

    Math.ceil() function added

--- math.js
+++ math.js
@@ -5,4 +5,8 @@ var sum = value1 + value2;
 
 console.log(sum);
 
-console.log("After rounding value is : "+ Math.round(sum));
+// console.log("After rounding value is : "+ Math.round(sum));
+
+console.log("After rounding value is : "+ Math.ceil(sum));
+
+
