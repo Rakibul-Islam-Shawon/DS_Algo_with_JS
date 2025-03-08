e98253107c7bfed1b8ea442b67384cdd9ea36eac
Rakibul Islam <rakibicenstu@gmail.com>
2025-03-08 15:50:37 +0100

    NOrmal mathmetic calculation

--- math.js
+++ math.js
@@ -12,3 +12,13 @@ console.log("After rounding (ceiling) value is : "+ Math.ceil(sum));
 console.log("After rounding (Floor) value is : "+ Math.floor(sum));
 
 
+// Exercise //
+
+var daal = 100;
+var sobji = 10;
+
+var icecream = 190;
+
+var total = daal * 1 + sobji * 2 + icecream * 1;
+
+console.log(`Total bill is : $${total}`);
